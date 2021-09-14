a4429c58eef80a9b7210cee051de04ae8e39fc05
TaoLoading <1377226380@qq.com>
2021-09-14 17:05:14 +0800

    登陆区和专栏信息区的实现

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <global-header :user="userData"></global-header>
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GolbalHeader.vue'
+// 专栏模拟数据
+const columnData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1专栏',
+    description: '这是test1专栏',
+    avatar: 'https://cn.vuejs.org/images/logo.svg'
+  },
+  {
+    id: 2,
+    title: 'test2专栏',
+    description: '这是test2专栏',
+    avatar: 'https://cn.vuejs.org/images/logo.svg'
+  },
+  {
+    id: 3,
+    title: 'test3专栏',
+    description: '这是test3专栏',
+    avatar: 'https://cn.vuejs.org/images/logo.svg'
+  },
+  {
+    id: 4,
+    title: 'test4专栏',
+    description: '这是test4专栏'
+    // avatar: 'https://cn.vuejs.org/images/logo.svg'
+  }
+]
+// 用户模拟数据
+const userData: UserProps = {
+  isLogin: true,
+  name: 'TaoLoading'
+}
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList,
+    GlobalHeader
+  },
+  setup() {
+    return {
+      list: columnData,
+      userData
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/GolbalHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
+    <a
+      href="#"
+      class="navbar-brand"
+    >BBS</a>
+    <ul
+      v-if="!user.isLogin"
+      class="list-inline mb-0"
+    >
+      <li class="list-inline-item">
+        <a
+          href="#"
+          class="btn btn-outline-light my-2"
+        >登陆</a>
+      </li>
+      <li class="list-inline-item">
+        <a
+          href="#"
+          class="btn btn-outline-light my-2"
+        >注册</a>
+      </li>
+    </ul>
+    <ul
+      v-else
+      class="list-inline mb-0"
+    >
+      <li class="list-inline-item">
+        <a
+          href="#"
+          class="btn btn-outline-light my-2"
+        >你好 {{user.name}}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang='ts'>
+import { defineComponent, PropType } from 'vue'
+export interface UserProps {
+  isLogin: boolean
+  name?: string
+  _id?: string
+  column?: string
+  email?: string
+  avatar?: string
+  description?: string
+}
+export default defineComponent({
+  name: 'GlobalHeader',
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  },
+  components: {}
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
